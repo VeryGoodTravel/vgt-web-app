d86d7dde130413e12c70e88e92e4479f4cf4a7f3
furkacz <100477457+furkacz@users.noreply.github.com>
2024-03-18 22:26:05 +0100

    Setting up project structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'airbnb-base',
+  ],
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+      },
+    },
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
+  plugins: [
+    'html',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.vue', '.js'],
+      },
+    },
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const packageJSON = require('./package.json');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Very Good Travel';
+      return args;
+    });
+    config.plugin('define').tap((args) => {
+      args[0]['process.env'].VUE_APP_VERSION = `"${packageJSON.version || '0.0.0'}"`;
+      return args;
+    });
+  },
+});
