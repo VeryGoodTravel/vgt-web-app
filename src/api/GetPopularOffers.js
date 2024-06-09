cf45918f2b68d8abef3a49628626923fca015ff3
furkacz <100477457+furkacz@users.noreply.github.com>
2024-06-09 21:32:31 +0200

    Removed unnecessary config parameters

--- src/api/GetPopularOffers.js
+++ src/api/GetPopularOffers.js
@@ -4,7 +4,7 @@ import endpoints from './endpoints';
 import errors from './errors';
 
 export default async () => {
-  const response = await api.get(endpoints.GetPopularOffers, { baseURL: process.env.VUE_APP_API_STATS_URL_VALUE });
+  const response = await api.get(endpoints.GetPopularOffers);
   if (!validators.responses.GetPopularOffers.isValid(response.data)) {
     throw new errors.SchemaParsingError('Error parsing GetPopularOffers response - invalid schema');
   } else {
